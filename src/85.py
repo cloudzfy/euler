91c6dba55784411ea974c080eac16660faf1e984
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-12-02 13:04:48 +0800

    Update to 85.

--- src/85.py
+++ src/85.py
@@ -15,3 +15,22 @@
 # exactly two million rectangles, find the area of the grid
 # with the nearest solution.
 
+n = 2000
+m = 1
+limit = 2000000
+count = 0
+diff = abs(limit - count)
+ans = 0
+
+while n >= m:
+	tmp = n * (n + 1) * m * (m + 1) / 4
+	if abs(tmp - limit) < diff:
+		diff = abs(tmp - limit)
+		count = tmp
+		ans = m * n
+	if tmp > limit:
+		n -= 1
+	else:
+		m += 1
+
+print ans
