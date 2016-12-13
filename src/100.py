d89dc62d86d215f52a57491bdd4cb85bcd145432
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-12-13 17:34:20 +0800

    Update to 100.
    Anniversary 100 solved problems!

--- src/100.py
+++ src/100.py
@@ -12,3 +12,13 @@
 # 10^12 = 1,000,000,000,000 discs in total, determine
 # the number of blue discs that the box would contain.
 
+x = 1
+y = 1
+limit = 1000000000000
+
+while True:
+	if x % 2 == 1 and y % 2 == 1:
+		if (x + 1) / 2 > limit:
+			print (y + 1) / 2
+			break
+	x, y = 3 * x + 4 * y, 2 * x + 3 * y
--- src/94.py
+++ src/94.py
@@ -10,3 +10,25 @@
 # with integral side lengths and area and whose perimeters do not
 # exceed one billion (1,000,000,000).
 
+limit = 1000000000
+
+x = 2
+y = 1
+ans = 0
+
+while True:
+	if 2 * x - 2 <= limit:
+		a = float(2 * x - 1) / 3
+		b = a - 1
+		if int(a) == a and a > 0 and b > 0:
+			ans += 2 * x - 2
+	if 2 * x + 2 <= limit:
+		a = float(2 * x + 1) / 3
+		b = a + 1
+		if int(a) == a and a > 1 and b > 0:
+			ans += 2 * x + 2
+	else:
+		break
+	x, y = 2 * x + y * 3, 2 * y + x
+
+print ans
